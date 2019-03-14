cd333bda6ff142b4a8ce41d686980ab0c8257f21
admin <admin>
2019-03-14 20:13:30 +0800

    第四次提交 lpc

--- src/main/java/com/baidu/test/controller/DeptController.java
+++ src/main/java/com/baidu/test/controller/DeptController.java
@@ -1,6 +1,8 @@
 package com.baidu.test.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping
@@ -13,6 +15,11 @@ public class DeptController {
 
 
         return "哈哈哈";
+    }
+
+    @RequestMapping("findById/{id}")
+    public String findById(@PathVariable("id") Integer id){
+        return "你的Id为"+id;
     }
 
 

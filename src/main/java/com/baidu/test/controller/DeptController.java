1e8befb8522d899309421e526a811931ccbfff79
xuxinke <1303310224@qq.com>
2019-03-14 20:08:21 +0800

    第4次提交  查询全部接口 findAll

--- src/main/java/com/baidu/test/controller/DeptController.java
+++ src/main/java/com/baidu/test/controller/DeptController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DeptController {
 
 
+    //查询全部
     @RequestMapping("findList")
     public String hh(){
 
@@ -15,5 +16,13 @@ public class DeptController {
         return "哈哈哈";
     }
 
+    //查询全部2
+    @RequestMapping("findAll")
+    public  String findAll(){
+
+        //查询全部
+        return "又查询了全部";
+
+    }
 
 }
